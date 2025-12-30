bcfa60ca71750be8b9ce856f16129976280e4316
Saeed <hassanisaeed19@gmail.com>
2025-12-30 23:18:30 +0330

    refactor: change api name

--- src/core/urls.py
+++ src/core/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/users/', include('apps.users.api.urls')),
     path('api/orders/', include('apps.orders.api.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # Docs
